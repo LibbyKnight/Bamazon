b34384fe2806217d1635627c76532a6487f3464f
Bea Lopez <Bea Lopez>
2017-01-12 22:20:49 -0500

    working with checking db

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -11,25 +11,82 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
-    tableList();
+  	console.log('connected');
+  	
 });
 
-function tableList() {
-        connection.query('SELECT * FROM products', function(err, res) {
-        	inquirer.prompt({
-			name: "inputChoices",
-			type: "rawlist",
-			message: "What is the ID of the product you would like to buy?",
-			choices: [console.log(columns)]
-		})
+function itemList() {
+    connection.query('SELECT * FROM products', function(err, res) {
 
-        	for (var i = 0; i < res.length; i++) {
-                var columns = columnify([{
-  		  'Item ID': res[i].ItemID,
-		  'Product Name': res[i].ProductName,
-		  'Price': res[i].Price,
-		}]) 		 
-		
-          };       
-        });
-      };
+    	for (var i = 0; i < res.length; i++) {
+
+        var columns = columnify([{
+
+            'Item ID': res[i].ItemID,
+            'Product Name': res[i].ProductName,
+            'Price': res[i].Price,
+        }], {
+            minWidth: 20,
+            config: {
+                description: {
+                    maxWidth: 30
+                }
+            }
+            
+        })
+       console.log(columns);
+		choosingItem();
+   		}
+    })
+
+};
+
+
+function choosingItem () {
+
+			inquirer.prompt([{
+			name: "item",
+			type: "input",
+			message: "What is the ID of the product you would like to buy?",		
+			 validate: function(value) {
+                   if (!isNaN(value)) {
+                       return true;
+                   } else {
+                       console.log(" Please enter a valid ID number.");
+                   }
+
+               }
+        }]).then(function (answers) {
+    		itemQuantity(answers);
+});	
+	 
+};
+
+
+function itemQuantity () {
+
+			inquirer.prompt([{
+			name: "quantity",
+			type: "input",
+			message: "How many items would you like to buy?",
+
+			 validate: function(value) {
+                   if (!isNaN(value)) {
+                       return true;
+                   } else {
+                       console.log(" Please enter a valid quantity.");
+                   }
+               }
+        }]) .then(function (answers) {
+    		placeOrder(answers);
+});		   
+};
+
+function placeOrder (answers) {
+
+	connection.query('SELECT * FROM products WHERE ItemID = ?', {itemID: item}, function (err, res) {
+	}); 
+	console.log(res);
+}
+
+itemList();
